aa9eacd7bbea8b7e02e9b0c2bbb87bdaeb9b458f
Hạ Huy <hoanghuy24825@gmail.com>
2021-07-25 22:50:00 +0700

    fix font end

--- ElectronicDevice/Controllers/HomeController.cs
+++ ElectronicDevice/Controllers/HomeController.cs
@@ -8,20 +8,11 @@ namespace ElectronicDevice.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
+       
         public ActionResult Introduce()
         {
             return View();
         }
 
-        public ActionResult Contact()
-        {
-
-            return View();
-        }
     }
 }
