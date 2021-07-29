ab3f48f1cdd8a29d68ad6773020947edd1952e9a
Khánh Vũ <khanhlnlov3@gmail.com>
2021-07-29 23:51:09 +0700

    khanh

--- ElectronicDevice/Areas/Admin/Controllers/ProductController.cs
+++ ElectronicDevice/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using ElectronicDevice.DTO;
+using ElectronicDevice.Models;
+using ElectronicDevice.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +15,60 @@ namespace ElectronicDevice.Areas.Admin.Controllers
     [Authorize(Roles = "ADMIN")]
     public class ProductController : Controller
     {
+        private ElectronicDeviceDbContext db = new ElectronicDeviceDbContext();
+
         // GET: Admin/Product
         public ActionResult Index()
         {
-            return View();
+            var listProduct = db.Products.Select(c => c);
+            ViewBag.Category = new SelectList(db.Categories, "ID_Category", "Name");
+            return View(listProduct);
+        }
+
+        [HttpPost]
+        public ActionResult AddProducts(ProductDTO product)
+        {
+            Product pro = new Product();
+
+            if (product.Base64 != null)
+            {
+                string path = HttpContext.Server.MapPath("~/wwwroot/imageUpload/");
+                //string filename = DateTime.Now.ToString("yyyyMMddTHHmmss") + ".jpg";
+                if (!UploadFileUtil.SaveFile(product.Base64, product.Image, path))
+                {
+                    return null;
+                }
+            }
+            pro.ID_Category = product.ID_Category;
+            pro.Name = product.Name;
+            pro.Image = product.Image;
+            pro.Price = product.Price;
+            pro.Model = product.Model;
+            pro.Amount = product.Amount;
+            pro.Guarantee = product.Guarantee;
+            pro.Origin = product.Origin;
+            pro.Discount = product.Discount;
+            pro.ShortDescription = product.ShortDescription;
+            pro.Detail = product.Detail;
+            pro.Status = product.Status;
+
+            //pro.CreatedDate = product.CreatedDate;
+            //pro.CreatedBy = product.CreatedBy;
+            //pro.ModifiedDate = product.ModifiedDate;
+            //pro.ModifiedBy = product.ModifiedBy;
+            if (product.ID_Product == 0)
+            {
+                db.Products.Add(pro);
+            }
+            else
+            {
+                pro.ID_Product = product.ID_Product;
+                db.Entry(pro).State = EntityState.Modified;
+            }
+
+            db.SaveChanges();
+
+            return Json(pro, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ElectronicDevice/DTO/ProductDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElectronicDevice.DTO
+{
+    public class ProductDTO
+    {
+        public int ID_Product { get; set; }
+
+        public int ID_Category { get; set; }
+
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Guarantee { get; set; }
+
+        public string Origin { get; set; }
+
+        public short? Discount { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Detail { get; set; }
+
+        public bool Status { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public string Base64 { get; set; }
+
+    }
+}
